ca4fc242a56a1866739f7e3de80bf5f41ff8387d
jkumawat <jitendra87@gmail.com>
2014-07-03 22:20:18 +0200

    CataShare project Added

---
+++ CataShare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CataShare.Startup))]
+namespace CataShare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
